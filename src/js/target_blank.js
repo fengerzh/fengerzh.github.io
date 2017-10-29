21bf4b0687708fa95a50762273e2943724747a1c
fengerzh <buzz.zhang@gmail.com>
2017-10-29 14:34:19 +0800

    add rel=noopener

--- src/js/target_blank.js
+++ src/js/target_blank.js
@@ -3,6 +3,7 @@
   for (var i = 0, linksLength = links.length; i < linksLength; i++) {
     if (links[i].hostname != window.location.hostname) {
       links[i].target = '_blank';
+      links[i].rel = 'noopener';
     }
   }
 })();
