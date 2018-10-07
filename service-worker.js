edb7a44d779dcfe316b864abdd6c3ac62f2aa5d8
fengerzh <buzz.zhang@gmail.com>
2018-10-07 11:44:35 +0800

    new post

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,18 @@
-var cacheName = 'v7';
+var cacheName = 'v8';
 var filesToCache = [
   '/',
   '/assets/js/main.js',
   '/assets/css/main.css',
   '/assets/img/placeholder.png',
   '/assets/img/icons/preloader.svg',
-  '/assets/img/icons/read.svg'
+  '/assets/img/icons/read.svg',
 ];
 
 self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
       return cache.addAll(filesToCache);
-    })
+    }),
   );
 });
 
@@ -24,9 +24,9 @@ self.addEventListener('activate', function(e) {
           if (key !== cacheName) {
             return caches.delete(key);
           }
-        })
+        }),
       );
-    })
+    }),
   );
   return self.clients.claim();
 });
@@ -35,6 +35,6 @@ self.addEventListener('fetch', function(e) {
   e.respondWith(
     caches.match(e.request).then(function(response) {
       return response || fetch(e.request);
-    })
+    }),
   );
 });
