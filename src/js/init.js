a35a682f609563a03b5950f41731bbad9296b647
fengerzh <buzz.zhang@gmail.com>
2019-04-14 14:20:15 +0800

    change api key

--- src/js/init.js
+++ src/js/init.js
@@ -1,9 +1,13 @@
-if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+if (typeof firebase === 'undefined')
+  throw new Error(
+    'hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js'
+  );
 firebase.initializeApp({
-  "apiKey": "AIzaSyCx13-CU1uqE3xQWpx1yxNBujUgnNmVb1Q",
-  "databaseURL": "https://fengerzhcom.firebaseio.com",
-  "storageBucket": "fengerzhcom.appspot.com",
-  "authDomain": "fengerzhcom.firebaseapp.com",
-  "messagingSenderId": "84597767663",
-  "projectId": "fengerzhcom"
+  apiKey: 'AIzaSyBHQjxlE2uaJ0janHVGUMsXpPGggkh-XrQ',
+  // "apiKey": "AIzaSyCx13-CU1uqE3xQWpx1yxNBujUgnNmVb1Q",
+  databaseURL: 'https://fengerzhcom.firebaseio.com',
+  storageBucket: 'fengerzhcom.appspot.com',
+  authDomain: 'fengerzhcom.firebaseapp.com',
+  messagingSenderId: '84597767663',
+  projectId: 'fengerzhcom'
 });
