b508e144b82b35da4ae54a8d2218ebd13a303b21
fengerzh <buzz.zhang@gmail.com>
2019-07-02 17:54:10 +0800

    pushmonkey

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'v10';
+var cacheName = 'v11';
 var filesToCache = [
   '/',
   '/assets/js/main.js',
--- src/js/zmain.js
+++ src/js/zmain.js
@@ -60,43 +60,4 @@
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/service-worker.js');
   }
-
-  var messaging = firebase.messaging();
-  messaging.usePublicVapidKey(
-    'BCPMmXHYS2_ynQAcVaHiV4p0iYVQnudc-0kD24xPWytY1zFOmvRl7cg6vNoOfjUO8lFHr-OyncRt5UU06G-BUdM'
-  );
-
-  // 申请获得推送权限
-  messaging
-    .requestPermission()
-    .then(function() {
-      messaging
-        .getToken()
-        .then(function(currentToken) {
-          if (currentToken) {
-            if (
-              window.localStorage.getItem('firebaseTokenSentToServer') !== '1'
-            ) {
-              firebase
-                .database()
-                .ref('messagetokens')
-                .push()
-                .set(currentToken);
-              window.localStorage.setItem('firebaseTokenSentToServer', '1');
-            } else {
-            }
-          } else {
-            // Show permission UI.
-            // updateUIForPushPermissionRequired();
-            window.localStorage.setItem('firebaseTokenSentToServer', '0');
-          }
-        })
-        .catch(function(err) {
-          // showToken('Error retrieving Instance ID token. ', err);
-          window.localStorage.setItem('firebaseTokenSentToServer', '0');
-        });
-    })
-    .catch(function(err) {
-      console.log('Unable to get permission to notify.', err);
-    });
 })(Zepto, window);
